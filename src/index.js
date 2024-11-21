27e70f38e280d38334186ac2cf816856ed353e75
DESKTOP-IQA7J21\tnaml <long.tn@mor.com.vn>
2024-11-21 22:37:56 +0700

    Turn off CORS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
